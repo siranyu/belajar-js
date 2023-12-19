b2d9ceb2d09da96fb61d4ba1aacdda4abfc7f05d
siranyu <anyugantenk@gmail.com>
2023-12-19 09:39:36 +0700

    update For While & doWhile

--- tugas.js
+++ tugas.js
@@ -1,17 +1,67 @@
 //bilangan 1-100 ganjil genap
-for(let i = 1; i <= 100; i++){
-    if(i % 2 == 0){
-        document.write("<span>angka Genap : </span>" + i + "<br>");
-    } else{
-        document.write("<span>angka Ganjil : </span>" + i + "<br>")
+document.write("<h1>FOR (ganjil genap)</h1>")
+for (let iFor = 1; iFor <= 100; iFor++) {
+
+    if (iFor % 2 == 0) {
+        document.write("<span>angka Genap : </span>" + iFor + "<br>");
+    } else {
+        document.write("<span>angka Ganjil : </span>" + iFor + "<br>");
     }
 }
 
-document.write("<p>=======================================</p>")
+document.write("<h1>WHILE (ganjil genap)</h1>");
+let iWhile = 1;
+
+while (iWhile <= 100) {
+
+    if (iWhile % 2 == 0) {
+        document.write("<span>angka Genap : </span>" + iWhile + "<br>");
+    } else {
+        document.write("<span>angka Ganjil : </span>" + iWhile + "<br>");
+    }
+    iWhile++
+}
+
+document.write("<h1>DO WHILE (ganjil genap)</h1>");
+let iDoWhile = 1;
+
+do {
+    if (iDoWhile % 2 == 0) {
+        document.write("<span>angka Genap : </span>" + iDoWhile + "<br>");
+    } else {
+        document.write("<span>angka Ganjil : </span>" + iDoWhile + "<br>");
+    }
+    iDoWhile++
+} while (iDoWhile <= 100);
+
+
+document.write("<p>=============================================================================================</p>")
 
 //tahun kabisat
-for(let x = 2000; x <= 2024; x++){
-    if(x % 4 == 0){
-        document.write(x + "<span> merupakan tahun kabisat!</span><br>");
+document.write("<h1>FOR (tahun kabisat)</h1>");
+for (let xFor = 2000; xFor <= 2024; xFor++) {
+    if (xFor % 4 == 0) {
+        document.write(xFor + "<span> merupakan tahun kabisat!</span><br>");
     }
 }
+
+document.write("<h1>WHILE (tahun kabisat)</h1>");
+let xWhile = 2000;
+
+while (xWhile <= 2024) {
+    if (xWhile % 4 == 0) {
+        document.write(xWhile + "<span> merupakan tahun kabisat!</span><br>");
+    }
+    xWhile++;
+}
+
+document.write("<h1>DO WHILE (tahun kabisat)</h1>");
+
+let xDoWhile = 2000;
+do {
+    if (xDoWhile % 4 == 0) {
+        document.write(xDoWhile + "<span> merupakan tahun kabisat!</span><br>");
+    }
+    xDoWhile++
+
+} while (xDoWhile <= 2024);
